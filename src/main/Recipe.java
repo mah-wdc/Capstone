746f10572cfb4aa6353b8bb4bbe125f0f1ced0c7
bvanwaes <150636532+bvanwaes@users.noreply.github.com>
2023-11-14 16:31:08 -0500

    Update Recipe.java
    Added comments to the files

--- src/main/Recipe.java
+++ src/main/Recipe.java
@@ -2,41 +2,45 @@ package main;
 
 import java.util.ArrayList;
 
+
 public class Recipe {
 	private String recipeName;
 	private ArrayList<String> ingredients;
 	private String instructions;
 
+	//constructor for the class setting the name, ingredients, and instructions
 	public Recipe(String recipeName, ArrayList<String> ingredients, String instructions) {
 		this.recipeName = recipeName;
 		this.ingredients = new ArrayList<>(ingredients);
 		this.instructions = instructions;
 	}
 
+	//getter for Recipe name
 	public String getRecipeName() {
 		return recipeName;
 	}
-
+	//setter for recipe name
 	public void setRecipeName(String recipeName) {
 		this.recipeName = recipeName;
 	}
-
+	//getter for ingredients list
 	public ArrayList<String> getIngredients() {
 		return new ArrayList<>(ingredients);
 	}
-
+	//setter for ingredients list
 	public void setIngredients(ArrayList<String> ingredients) {
 		this.ingredients = new ArrayList<>(ingredients);
 	}
-
+	//getter for instructions String
 	public String getInstructions() {
 		return instructions;
 	}
-
+	//setter for instruction string
 	public void setInstructions(String instructions) {
 		this.instructions = instructions;
 	}
 
+	//toString overriddent to accomodate the creation of the Recipe name, ingredients, and instructions
 	@Override
 	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder();
@@ -46,6 +50,7 @@ public class Recipe {
 		return stringBuilder.toString();
 	}
 
+	//Using the String found in the file, separates the 3 components of the String into its separate components
 	public static Recipe parseRecipeFromString(String recipeString) {
 		// Split the input string by newline character to get different fields
 		String[] fields = recipeString.split("\n");

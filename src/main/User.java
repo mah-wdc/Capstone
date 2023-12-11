530a8e971cf01dd748ee43e8915714ef7731971b
bvanwaes <150636532+bvanwaes@users.noreply.github.com>
2023-12-11 16:03:20 -0500

    Update User.java
    Add comments

--- src/main/User.java
+++ src/main/User.java
@@ -1,14 +1,23 @@
 package main;
 
+/*
+*   File: User.java
+*   Authors: Matthew Homan, Bethany Van Waes, and Michael Rach
+*   Date: 12/12/2023
+*   Purpose: This file creates the User object for signing into the program. 
+*/
+
 public class User {
 	private String name;
 	private String password;
 
+	//constructor sets name and password
 	public User(String name, String password) {
 		this.name = name;
 		this.password = password;
 	}
 
+	//getter for getting name
 	public String getName() {
 		return name;
 	}
